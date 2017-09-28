7ea29b4dd9ce452ff73c164bbfc003d71dc063b2
CristalleGanter <S01059084@acad.tri-c.edu>
2017-09-28 12:38:36 -0400

    added class

---
+++ ClassTask/ClassTask/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tree Maple = new Tree();
+            Maple.numBranches = 25;
+
+        }
+    }
+}
---
+++ ClassTask/ClassTask/Trees.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassTask
+{
+    class Tree
+    {
+       public double numBranches;
+        private string color;
+        private bool alive;
+        private int roots;
+
+    }
+    
+    }
+
