7c8935a2c2f82dcd91efcd6a701e1205335a582b
cristalleganter <S01059084@acad.tri-c.edu>
2017-10-20 12:11:35 -0400

    finally figured out how to use user input if it's string

---
+++ ApplePicking/ApplePicking/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplePicking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int app = 0;
+            Console.WriteLine("You have {0} apples.", app);
+            Console.WriteLine("\n Would you like to pick apples?");
+            string ans;
+            ans = Console.ReadLine();
+
+            if (ans == "y")
+            { app++; Console.WriteLine("You have {0} apple.", app); }
+
+            if (ans == "n")
+            { Console.WriteLine("You have {0} apples.", app); }
+
+
+        }
+    }
+}
