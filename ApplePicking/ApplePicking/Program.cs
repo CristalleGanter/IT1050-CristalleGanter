6204519f77676b1645e67e1ac9730b17835c3846
cristalleganter <S01059084@acad.tri-c.edu>
2017-10-20 13:20:45 -0400

    added apple pie

--- ApplePicking/ApplePicking/Program.cs
+++ ApplePicking/ApplePicking/Program.cs
@@ -24,7 +24,7 @@ namespace ApplePicking
             { app++; Console.WriteLine("You have {0} apple.", app); }
 
             if (ans == "n")
-            { Console.WriteLine("You have {0} apples.", app); }
+            { Console.WriteLine("You still have {0} apples.", app); }
 
             Console.WriteLine("Would you like to pick apples?");
             string ans2;
@@ -34,7 +34,7 @@ namespace ApplePicking
             { app++; Console.WriteLine("You have {0} apples.", app); }
 
             if (ans2 == "n")
-            { Console.WriteLine("You have {0} apples.", app); }
+            { Console.WriteLine("You still have {0} apples.", app); }
 
             while (app < 10)
             {
@@ -46,11 +46,27 @@ namespace ApplePicking
                 { app++; Console.WriteLine("You have {0} apples.", app); }
 
                 if (ans3 == "n")
-                { Console.WriteLine("You have {0} apples.", app); }
+                { Console.WriteLine("You still have {0} apples.", app); }
             }
             if (app>=10)
-            { Console.WriteLine("Your basket is full."); }
+            { Console.WriteLine("Your basket is full.");
+                Console.WriteLine("Would you like to make pie?");
+                string pie;
+                pie = Console.ReadLine();
+
+                if (pie == "y")
+                { app=0; Console.WriteLine("You have {0} apples and 1 pie.", app);
+                    Console.WriteLine("  S    S    S\n  S    S    S \n  \n  ~~~~~~~~~~~\n (###########)\n  \\_________/");
+                }
+
+                if (pie == "n")
+                { Console.WriteLine("Your basket is full."); }
+
+
+
+            }
             
+
 
         }
     }
