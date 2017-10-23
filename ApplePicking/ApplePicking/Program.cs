a9de142eaae54ff537dcf579864d5d256e2b3ad1
cristalleganter <S01059084@acad.tri-c.edu>
2017-10-23 19:48:16 -0400

    trying to figure out how to use random numbers to control chances of console.writeline running

--- ApplePicking/ApplePicking/Program.cs
+++ ApplePicking/ApplePicking/Program.cs
@@ -14,11 +14,20 @@ namespace ApplePicking
             Console.WriteLine("\n    ~~~~~~ \n   (      ) \n  (        )\n  (        )\n   (      )\n     |  |\n     |  |\n     |  |\n     |  |");
             Console.WriteLine("\n    ~~~~~    \n  (       ) \n (         )\n (         )\n  ~~~~~~~~~\n  ~~~~~~~~~\n  ~~~~~~~~~\n  ~~~~~~~~~");
 
+
+
+          
             int app = 0;
             Console.WriteLine("You have {0} apples.", app);
             Console.WriteLine("\nWould you like to pick apples?");
             string ans;
             ans = Console.ReadLine();
+
+            Random bunny = new Random();
+            int randombunny = bunny.Next(0, 25);
+            if (randombunny == 25)
+            { app = 0; Console.WriteLine("Oh no! A bunny ate all of your apples! You have {0} apples.", app); }
+
 
             if (ans == "y")
             { app++; Console.WriteLine("You have {0} apple.", app); }
