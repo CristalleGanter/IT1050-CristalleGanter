0e29b9c94b24db1c2abed7775e6cff521c0ed087
cristalleganter <S01059084@acad.tri-c.edu>
2017-10-25 20:16:44 -0400

    finished question 1

---
+++ CristalleGanterLab7/CristalleGanterLab7/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CristalleGanterLab7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n =1;
+            Console.WriteLine(n);
+
+            while (n<9)
+            { Console.WriteLine(n + 2); n = n + 2; }
+
+
+           
+
+        }
+    }
+}
