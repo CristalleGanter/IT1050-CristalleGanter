9b9630b37c1fc7b77462fa7c976273722679801d
cristalleganter <S01059084@acad.tri-c.edu>
2017-11-07 12:44:31 -0500

    started number 3

--- CristalleGanterIT1050Test1/CristalleGanterIT1050Test1/Program.cs
+++ CristalleGanterIT1050Test1/CristalleGanterIT1050Test1/Program.cs
@@ -10,6 +10,12 @@ namespace CristalleGanterIT1050Test1
     {
         static void Main(string[] args)
         {
+            // number 1
+
+
+
+
+            // number 2
             int x = 0;
             int counter = 0;
             while (x != 128)
@@ -21,6 +27,26 @@ namespace CristalleGanterIT1050Test1
                 Console.WriteLine("[{0}]\n",x);
                 Console.WriteLine(counter);
             }
+            Console.WriteLine("\n");
+
+            // number 3
+
+            for (int y = 49; y >= 1; y--)
+            {
+                Console.WriteLine("{0}",y); if (y != 1) { Console.WriteLine(","); }
+                
+            }
+
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
