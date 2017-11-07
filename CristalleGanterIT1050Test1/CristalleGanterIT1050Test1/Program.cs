6cf342c2267f8f62b5c4a6171e5f8b68ccb46863
cristalleganter <S01059084@acad.tri-c.edu>
2017-11-07 12:20:05 -0500

    working on question 2

---
+++ CristalleGanterIT1050Test1/CristalleGanterIT1050Test1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CristalleGanterIT1050Test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            
+            while (x != 128)
+
+            {
+                int counter = 1;counter++;
+                x = x + 2;
+
+                Console.WriteLine("[{0}]\n",x); }
+        }
+    }
+}
