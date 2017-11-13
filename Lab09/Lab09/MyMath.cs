fbf6ae88e67f896beee9a13266427855ec8a1ebf
cristalleganter <S01059084@acad.tri-c.edu>
2017-11-13 11:58:32 -0500

    started lab9

---
+++ Lab09/Lab09/MyMath.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab09
+{
+    class MyMath
+    {
+        double result;
+        double operand1;
+        double operand2;
+
+        public void Multiply(double operand1, double operand2)
+        { }
+
+    }
+}
