1ea5afa85d4c6b207ae98224dffdad5489e8d5c2
cristalleganter <S01059084@acad.tri-c.edu>
2017-11-30 18:48:33 -0500

    started lab 13

---
+++ GanterLab13/GanterLab13/Car.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GanterLab13
+{
+    class Car
+    {
+        private int Speed;
+        private string Make;
+        private string Model;
+        private int Year;
+
+
+
+    }
+}
