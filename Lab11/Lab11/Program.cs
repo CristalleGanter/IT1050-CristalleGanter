d2a32da1bc3c2046e5d5441d20cc71be039de051
cristalleganter <S01059084@acad.tri-c.edu>
2017-12-03 18:57:55 -0500

    finished question 1

---
+++ Lab11/Lab11/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] names = { "Al Dente", "Anna Graham", "Earle Bird", "Ginger Rayle", "Iona Ford" };
+            names[0] = "Al Dente";
+            names[1] = "Anna Graham";
+            names[2] = "Earle Bird";
+            names[3] = "Ginger Rayle";
+            names[4] = "Iona Ford";
+
+            Console.WriteLine(names[2]+ names[4]);
+
+        }
+    }
+}
