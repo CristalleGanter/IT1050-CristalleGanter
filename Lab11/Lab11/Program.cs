0fba5d1966db9258d7038ec05e1eed4d156db898
cristalleganter <S01059084@acad.tri-c.edu>
2017-12-03 19:05:11 -0500

    started question 2

--- Lab11/Lab11/Program.cs
+++ Lab11/Lab11/Program.cs
@@ -10,7 +10,7 @@ namespace Lab11
     {
         static void Main(string[] args)
         {
-
+            // Question 1
             string[] names = { "Al Dente", "Anna Graham", "Earle Bird", "Ginger Rayle", "Iona Ford" };
             names[0] = "Al Dente";
             names[1] = "Anna Graham";
@@ -19,6 +19,11 @@ namespace Lab11
             names[4] = "Iona Ford";
 
             Console.WriteLine(names[2]+ names[4]);
+
+            // Question 2
+
+            string[] months = { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
+
 
         }
     }
