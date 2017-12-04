797dda06fe6a1f58c23d66baee15def7c9ed2b96
cristalleganter <S01059084@acad.tri-c.edu>
2017-12-03 19:11:41 -0500

    finished number 2

--- Lab11/Lab11/Program.cs
+++ Lab11/Lab11/Program.cs
@@ -23,8 +23,30 @@ namespace Lab11
             // Question 2
 
             string[] months = { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
+            months[0] = "January";
+            months[1] = "February";
+            months[2] = "March";
+            months[3] = "April";
+            months[4] = "May";
+            months[5] = "June";
+            months[6] = "July";
+            months[7] = "August";
+            months[8] = "September";
+            months[9] = "October";
+            months[10] = "November";
+            months[11] = "December";
+
+            for (int i = 0; i < 12; i++)
+            {
+                Console.WriteLine(months[i]);
+            }
+
+
+
+
 
 
         }
+
     }
 }
